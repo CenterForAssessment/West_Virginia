e8ef4ae6b65d70f3b9edccf725c4db43faf610f9
dbetebenner <dbetebenner@gmail.com>
2015-09-15 12:22:07 -0400

    Update of 2014_2015 SGP script

---
+++ West_Virginia_SGP_2014_2015.R
@@ -0,0 +1,30 @@
+#####################################################################################
+###
+### West Virginia analyses for 2014-2015
+###
+#####################################################################################
+
+### Load SGP Package
+
+require(SGP)
+
+
+### Load data
+
+load("Data/West_Virginia_SGP.Rdata")
+load("Data/West_Virginia_Data_LONG_2014_2015.Rdata")
+
+
+### updateSGP
+
+West_Virginia_SGP <- updateSGP(
+	West_Virginia_SGP, 
+	West_Virginia_Data_LONG_2014_2015,
+	sgPlot.demo.report=TRUE,
+	save.intermediate.results=TRUE,
+	parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=8, PROJECTIONS=8, BASELINE_PERCENTILES=8, LAGGED_PROJECTIONS=8, SUMMARY=24, GA_PLOTS=2, SG_PLOTS=1)))
+
+
+### Save results
+
+save(West_Virginia_SGP, file="Data/West_Virginia_SGP.Rdata")
